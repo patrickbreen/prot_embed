2154b13754b5bb6d600b9666f2fd88558dfd6a69
patrickbreen <pbreen@uga.edu>
2016-04-27 23:08:22 -0400

    have working tutorials

---
+++ data/download_commands.sh
@@ -0,0 +1,9 @@
+# for downloading all bacteria proteins:
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/bacteria/*.faa.gz
+
+# for downloading all bacteria rna:
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/bacteria/*.rna.fna.gz
+
+# for downloading all bacteria contigs:
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/bacteria/*genomic.fna.gz
+
